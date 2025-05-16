e703c8d83b23cdef82089dc4e7002f29a1279f81
YASH <yash1608.sinha@gmail.com>
2025-05-16 12:27:23 +0530

    update

--- archery.cpp
+++ archery.cpp
@@ -1,25 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter the number of shots taken";
-    cin>>n;
-    int shot[n];
-    cout<<"enter the each value of score btw 0 to 10";
-    for(int i=0;i<n;i++){
-        cin>>shot[i];
+  int n;
+  cout<<"enter the number of shots taken(should be less than 100)";
+  cin>>n;
+  int shot[n];
+  cout<<"enter the value of each shot made (value should be btw 0 to 10)";
+  for(int i=0;i<n;i++){
+    cin>>shot[i];
+  }
+  int goodshot=0;
+  int badshot=0;
+  for (int i=0;i<n;i++){
+    if(shot[i]>=7){
+      goodshot++;
     }
-    int goodshot=0;
-    int badshot=0;
-    for(int i=0;i<n;i++){
-        if(shot[i]>=7){
-            goodshot++;
-        }
-        else{
-            badshot++;
-        }
+    else{
+      badshot++;
     }
-        cout << goodshot << " " << badshot;
-
-    return 0;
+  }
+  cout<<goodshot<<" "<<badshot;
+ return 0;
 }
