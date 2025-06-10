ebc5119497e41279f8a3e00d65cb72a03bfc5aae
YASH <yash1608.sinha@gmail.com>
2025-06-10 16:51:22 +0530

    solved

--- second_largest.cpp
+++ second_largest.cpp
@@ -1,2 +1,23 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int n;
+    cin>>n;
+  int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    int largest =arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    } 
+    int second_largest=-1;
+    for(int i=0;i<n;i++){
+        if(second_largest<arr[i] && arr[i]<largest){
+            second_largest=arr[i];
+        }
+    }
+    cout<<second_largest;
+}
