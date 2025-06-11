edaba7beb2f68c424735af9fdbaef17332ac8911
YASH <yash1608.sinha@gmail.com>
2025-06-11 19:52:07 +0530

    solved own

--- left_rotate.cpp
+++ left_rotate.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+/*int main(){
     int n;
     cin>>n;
   int arr[n];
@@ -13,6 +13,31 @@ int main(){
         }
         arr[n-1]=temp;
         for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    } 
+}*/
+    //left rotate by d number of places 
+    int main(){
+        int d;
+        cout<<"enter the number you want to rotate the array";
+        cin>>d;
+        int temp[d];
+        int n;
+    cin>>n;
+  int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    for(int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    for(int i=d;i<n;i++){
+        arr[i-d]=arr[i];
+    }
+    for(int i=0;i<d;i++){
+        arr[n-1-i] = temp[i];
+    }
+    for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     } 
 }
