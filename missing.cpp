87ba19ae4e9f806814164cba85e7c5fa7727ccf0
YASH <yash1608.sinha@gmail.com>
2025-06-12 14:11:44 +0530

    S

--- missing.cpp
+++ missing.cpp
@@ -20,7 +20,7 @@ for(i=1;i<=n;i++){
         return i;
     }
 */
-int hash[n+1]={0};
+/*int hash[n+1]={0};
 for(int i=0;i<n;i++){
     hash[arr[i]] +=1;
 }
@@ -28,6 +28,13 @@ for(int i=1;i<=n;i++){
     if(hash[i]==0){
         return i;
     }
+}*/
+int sum=n*(n+1)/2;
+int s2=0;
+for(int i=0;i<n;i++){
+    s2=arr[i]+s2;
 }
+return (sum-s2);
+
 }
 
