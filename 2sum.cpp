c78b5320134ef012934eb5d698fd6fb6cbe4fc38
YASH <yash1608.sinha@gmail.com>
2025-06-17 17:29:07 +0530

    S

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,16 +1,31 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-     int n,sum;
+     int n,sum,target;
     cin>>n;
   int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
-    }  for(int i=0;i<n-1;i++){
+    }  
+   /* for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             if(arr[i]+arr[j]==sum){
                 cout<<"YES";
+                break;
             }
         }
+    }*/
+int left=0;
+int right=n-1;
+sort(arr,arr+n);
+while(left<right){
+    int sum=arr[left]+arr[right];
+    if(sum==target){
+        return 1;
     }
+    else if(sum<target) left++;
+    else right--;
+}
+return 0;
 }
