913bf284c35de8e32cee2775f270c81a12e8f144
YASH <yash1608.sinha@gmail.com>
2025-06-18 16:26:20 +0530

    S

---
+++ kadanaldo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    int max=INT16_MIN;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if(sum>max){
+            max=sum;
+        }
+
+        if(sum<0){
+            sum=0;
+        }
+    }
+    if(max<0){
+        max=0;
+    }
+    cout<<max;
+}
---
+++ majority.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,count,el;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+       for(int i=0;i<n;i++){
+        if(count==0){
+            count=1;
+            el=arr[i];
+        }
+        else if(arr[i]==el){
+            count++; 
+        }
+        else{
+            count--;
+        }
+       }
+       int counter1=0;
+       for(int i=0;i<n;i++){
+        if(arr[i]==el){
+            counter1++;
+        }
+        if(count>n/2){
+            return el;
+        }
+    return -1;
+       }
+}
