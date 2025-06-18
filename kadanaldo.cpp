a20ca20a9d1c35aa569debc881dd81eada2cf3ba
YASH <yash1608.sinha@gmail.com>
2025-06-18 16:40:15 +0530

    S

--- kadanaldo.cpp
+++ kadanaldo.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,sum;
+    int n,sum,start,ansstart,ansend;
     cin>>n;
   int arr[n];
   for(int i=0;i<n;i++){
@@ -9,9 +9,13 @@ int main(){
     } 
     int max=INT16_MIN;
     for(int i=0;i<n;i++){
+        if(sum==0){
+            start=i;
+        }
         sum=sum+arr[i];
         if(sum>max){
             max=sum;
+            ansstart=start,ansend=i;
         }
 
         if(sum<0){
