1d10392f46019a0b878634344743d51483d546df
YASH <yash1608.sinha@gmail.com>
2025-06-20 01:47:23 +0530

    s

--- leader.cpp
+++ leader.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
     int n;
+    int max=INT16_MIN;
+    vector<int> ans;
     cin>>n;
   int arr[n];
   for(int i=0;i<n;i++){
         cin>>arr[i];
     } 
+for(int i=n-1;i>=0;i--){
+    if(arr[i]>max){
+ans.push_back(arr[i]);
+max=arr[i];
+    }
+}
+
+}
