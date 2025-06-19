efece62cc6269d76c52ee0991f500c02058dc928
YASH <yash1608.sinha@gmail.com>
2025-06-20 00:49:36 +0530

    s

---
+++ .vscode/permutation.cpp
@@ -0,0 +1,16 @@
+ //1.Find break index
+ //2.find number just greater than the break index 
+ //3.sort the baki ka remaining array / reverse
+ 
+ #include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+
+}
+ 
