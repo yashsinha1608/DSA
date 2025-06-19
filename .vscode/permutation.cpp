f1adb3ee9d405d6c105d0a89afa8ffabec4f1f3b
YASH <yash1608.sinha@gmail.com>
2025-06-20 01:09:32 +0530

    s

--- .vscode/permutation.cpp
+++ .vscode/permutation.cpp
@@ -1,8 +1,9 @@
  //1.Find break index
  //2.find number just greater than the break index 
  //3.sort the baki ka remaining array / reverse
- 
+
  #include<iostream>
+ #include<algorithm>
 using namespace std;
 int main(){
     int n;
@@ -11,6 +12,22 @@ int main(){
   for(int i=0;i<n;i++){
         cin>>arr[i];
     } 
+int index=-1;
+for(int i=n-2;i>=0;i--){            //first step is complete 
+    if(arr[i]<arr[i+1]){
+        index=i;
+        break;
+    }
+}
+if(index==-1){
+    reverse(arr,arr+n);
+}
+    for (int i=n-1;i>=index;i--){
+        if(arr[i]>arr[index]){
+            swap(arr[i],arr[index]);
+        }
+    }
+    reverse(arr+index,arr+n);
 
 }
  
