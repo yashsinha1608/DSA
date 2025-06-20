35a77c717cc045574b889cdbda3b68a3d486f609
YASH <yash1608.sinha@gmail.com>
2025-06-21 00:37:23 +0530

    s

---
+++ setmatrix.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n;
+    cin>>m;
+    int arr[n][m];
+    int row[n];
+    int col[m];
+  int arr[n][m];
+  for(int i=0;i<n;i++){
+    for (int j=0;j<m;j++){
+         cin>>arr[i][j];
+    }
+    } 
+    for(int i=0;i<n;i++){
+    for (int j=0;j<m;j++){
+        if(arr[i][j]==0){
+            row[i]=1;
+            col[j]=1;
+        }
+    }
+    }
+    for(int i=0;i<n;i++){
+    for (int j=0;j<m;j++){
+        if(row[i] || row[j]==1){
+            arr[i][j]=0;
+        }
+
+    }
+    } 
+
+}
