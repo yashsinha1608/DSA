a66648e796f7113935b521175e6a678506bc378a
YASH <yash1608.sinha@gmail.com>
2025-06-23 00:20:46 +0530

    s

---
+++ majority_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,count,el;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    //not understood pending 
+}
