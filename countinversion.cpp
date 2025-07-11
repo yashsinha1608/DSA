18b15e8b59ae156bf26b9453b847f39b4767a78e
YASH <yash1608.sinha@gmail.com>
2025-07-11 17:23:44 +0530

    new

---
+++ countinversion.cpp
@@ -0,0 +1,16 @@
+//find the number of pair suh that the left element is > the right element 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n]; 
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }   
+
+
+}
