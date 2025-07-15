ce2ff1d6abad05cb2813813117937ce31d590ffa
YASH <yash1608.sinha@gmail.com>
2025-07-15 17:06:56 +0530

    BS

---
+++ Binary_search.cpp/bs4.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int search(int* arr,int n,int k){
+int low=0;
+int high=n-1;
+while(low<=high){
+int mid=(high+low)/2;
+if(arr[mid]==k) return mid;
+
+if(arr[low]<=arr[mid]){//left sorted
+
+if(arr[low]<=k && k<=arr[mid]){//target is btw low and mid ie target is on the left hand side
+    high=mid-1;// we eleminated the RHS   
+}
+else{
+    low=mid+1;// elemenitaed the LHS
+}
+}
+else{// right sorted 
+if(arr[mid]<=k&&k<=high){//target is in RHS
+    low=mid+1;//eliminated LHS
+}
+else{
+    high=mid-1;//eleiminated rhs
+}
+
+}
+}
+return -1;
+}
+
+
+
+int main(){
+    int n,k;
+    cin >> n;
+    cin>>k;
+    int arr[n]; 
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int ans=search(arr,n,k);
+}
