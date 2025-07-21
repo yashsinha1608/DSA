335cf46e5d6b4bfec2a97dffb161a47d03c7619b
YASH <yash1608.sinha@gmail.com>
2025-07-21 20:24:30 +0530

    BS

---
+++ bs18.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k;
+    cin >> n;
+
+    cin>>k;
+    int arr[n]; 
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+}
