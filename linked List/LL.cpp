fdd9763fc9bb4ff8e4ca13a6f5ab4a4013458f2b
YASH <yash1608.sinha@gmail.com>
2025-08-21 00:34:19 +0530

    s

--- linked List/LL.cpp
+++ linked List/LL.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class node{
+    public:
     int data;
     node* next;
 public:
@@ -16,8 +18,47 @@ public:
     }
 
 };
+// this fucion is for creation of LL when array is given 
+node* convert(vector<int> &arr){
+    node* head=new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp; 
+        mover = temp;
+    }
+    return head;
+}
+int lenghth(node* head){
+    int cnt=0;
+    node* temp=head;
+  while(temp){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    cnt++;
+  }
+  return cnt;
 
+}
+
+int present(node* head,int val){
+    node* temp=head;
+  while(temp){
+    if(temp->data==val){
+        return 1;
+    }
+    temp=temp->next;
+  }
+  return 0;
+}
 int main(){
-    node* y=new node(2);
-    cout<<y;
+  vector<int> arr={12,5,7,6,4};
+  node* head= convert(arr);
+  node* temp=head;
+  while(temp){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<lenghth(head);
+  cout<<present(head,5);
 }
