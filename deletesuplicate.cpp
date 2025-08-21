f3f4edb56042f0e53aa3ea1a06ed2010d06eb0b2
YASH <yash1608.sinha@gmail.com>
2025-08-22 01:33:25 +0530

    s

--- deletesuplicate.cpp
+++ deletesuplicate.cpp
@@ -1,34 +1,33 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
+#include<unordered_set>
 using namespace std;
 
-vector<int> find_duplicate(vector<int>& arr){
-unordered_map<int,int> mp;
-vector<int> ans;
+vector<int> delete_duplicate(vector<int>& arr){
+    unordered_set<int> set;
+    vector<int> ans;
+    for(int x:arr){
+        if(set.find(x)==set.end()){
+            ans.push_back(x);
+            set.insert(x);
+        }
+    }
 
-for(int i=0;i<arr.size();i++){
-    mp[arr[i]]++;
-}
-for(auto &entry:mp){
-    ans.push_back(entry.first);
-}
-return ans;
+    return ans;
 }
 
 
 int main(){
     int n;
-    cout<< "enter size of array";
     cin>>n;
-vector<int> arr(n);
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-}
-
-vector<int> result=find_duplicate(arr);
-for(int i:result){
-    cout<<i<<" ";
-}
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    vector<int> answer=delete_duplicate(arr);
+    for(int x:answer){
+        cout<<x<<" ";
+    }
 
 }
