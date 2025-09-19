d3685630112d7ade311a3f7da94b49d5d01d9dd8
YASH <yash1608.sinha@gmail.com>
2025-09-19 16:25:17 +0530

    S

---
+++ Recursion/R_pow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+double pow(double x, int n) {  // return type should be double
+    double ans = 1.0;
+    long long nn = n;
+    if (nn < 0) nn = -nn;
+
+    while (nn) {
+        if (nn % 2) {          // odd power
+            ans = ans * x;
+            nn--;
+        } else {               // even power
+            x = x * x;
+            nn /= 2;
+        }
+    }
+
+    if (n < 0) ans = 1.0 / ans;
+    return ans;
+}
+
+int main() {
+    double base;
+    int power;
+    cin >> base >> power;
+
+    double ans = pow(base, power);
+    cout << ans;
+    return 0;
+}
