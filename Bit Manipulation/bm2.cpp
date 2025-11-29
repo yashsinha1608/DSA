23cc307b579fa2c7e043649e86ff376216a71d90
YASH <yash1608.sinha@gmail.com>
2025-11-29 21:23:17 +0530

    s

---
+++ Bit Manipulation/bm2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+long function(int divident,int divisor ){
+    if (divident==divisor) return 1;
+
+bool sign=true;
+if(divident>=0 && divisor <0) sign =false;
+if(divident<0 && divisor >0 ) sign = false;
+
+long n=abs(divident);
+long d=abs(divisor);
+long  ans=0;
+while(n>=d){
+     int count=0;
+    while(n>= (d<<(count+1))){
+        count++;
+    }
+    ans=ans+ 1<<count;
+    n=n-(d<<count);
+}
+
+if(ans>= 1<<31 && sign==true)
+return INT32_MAX;
+if(ans>=1<<31 && sign==false)
+return INT32_MIN;
+
+if(sign==true)
+return ans;
+else{
+    return (-1*ans);
+}
+
+
+}
+
+
+
+
+int main(){
+
+}
