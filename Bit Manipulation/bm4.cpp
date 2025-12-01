ad666a28bfd427581fd56ea1c60dc9a154288236
YASH <yash1608.sinha@gmail.com>
2025-12-02 00:08:16 +0530

    s

--- Bit Manipulation/bm4.cpp
+++ Bit Manipulation/bm4.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+
+ int singleNumber(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            x= x ^ nums[i];
+        }
+        return x;
+    }
+
 int main(){
-    
+
 }
