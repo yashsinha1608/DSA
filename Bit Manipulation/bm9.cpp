ee9e28b4cbcaa13daefec425b041cf2faf67b129
YASH <yash1608.sinha@gmail.com>
2025-12-02 23:31:10 +0530

    s

---
+++ Bit Manipulation/bm9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> divisors(int n){
+    vector<int> ans;
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0){
+            ans.push_back(i);
+            if(n/i!=i){
+                ans.push_back(n/i);
+            }
+        }
+    }
+    return ans;
+}
