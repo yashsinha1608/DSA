43214b8f031364cbfea4ec4b8d02f67b5ae53aa8
YASH <yash1608.sinha@gmail.com>
2025-12-05 00:24:51 +0530

    updated

--- Stack and Queue/Check_for_balanced_paranthesis.cpp
+++ Stack and Queue/Check_for_balanced_paranthesis.cpp
@@ -4,6 +4,27 @@
 #include<algorithm>
 using namespace std;
 
-int main(){
+bool isValid(string s) {
+         stack<char> st;
+         for(auto it:s){
+            if(it=='('|| it=='{'|| it=='['){
+                st.push(it);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch=st.top();
+                st.pop();
+                if((it==')' && ch=='(') || (it=='}'&& ch=='{')||(it==']'  && ch=='[')){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+         }
+         return st.empty();
+    }
     
+int main(){
+
 }
