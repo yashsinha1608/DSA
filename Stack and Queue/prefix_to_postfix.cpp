b28f1bc7620d23fe604c3ae84f9667e9ac323339
YASH <yash1608.sinha@gmail.com>
2025-12-10 00:03:11 +0530

    s

---
+++ Stack and Queue/prefix_to_postfix.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to convert prefix to postfix
+string prefixToPostfix(string prefix) {
+    stack<string> s;
+    int n = prefix.size();
+
+    // Traverse the prefix expression from right to left
+    for (int i = n - 1; i >= 0; i--) {
+        char c = prefix[i];
+
+        // If the character is an operand, push it to the stack
+        if (isalnum(c)) {
+            s.push(string(1, c));
+        } else {
+            // Pop two operands from the stack
+            string op1 = s.top(); s.pop();
+            string op2 = s.top(); s.pop();
+
+            // Form the new postfix expression and push back to stack
+            s.push(op1 + op2 + c);
+        }
+    }
+
+    // The final element in the stack is the result
+    return s.top();
+}
+int main(){
+    
+}
