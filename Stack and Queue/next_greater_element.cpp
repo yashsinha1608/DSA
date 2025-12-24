5b15861eb6c49b8868880790d82a154419a0c49a
YASH <yash1608.sinha@gmail.com>
2025-12-25 00:10:44 +0530

    S

--- Stack and Queue/next_greater_element.cpp
+++ Stack and Queue/next_greater_element.cpp
@@ -1,7 +1,26 @@
  #include <bits/stdc++.h>
 using namespace std;
 
-
+ vector<int> nextGreater(vector<int>& nums){
+    stack<int> st;
+    
+    int n=nums.size();
+     vector<int> nge(n);
+    for (int i=n-1;i>=0;i--){
+        
+        while(!st.empty() && st.top()<=nums[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            nge[i]=-1;
+        }
+        else{
+            nge[i]=st.top();
+        }
+        st.push(nums[i]);
+    }
+    return nge;
+ }
 
 int main(){
 
