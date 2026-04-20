34968d7788ac39038190a365c6e4427c3bac9308
YASH <yash1608.sinha@gmail.com>
2026-04-20 21:10:53 +0530

    s

--- sliding window&2pointer/longest_substring_norepeat.cpp
+++ sliding window&2pointer/longest_substring_norepeat.cpp
@@ -8,16 +8,19 @@ using namespace std;
         int l=0;
         int r=0;
         int maxlen=0;
-        while(r<n){
-            if(hash[s[r]]!=-1){
-                 l = max(hash[s[r]] + 1, l);
-            }
-             int len = r - l + 1;
-             maxlen=max(len,maxlen);
-             hash[s[r]]=r;
-             r++;
-        }
-        return maxlen;
+      while(r<n){
+          if(hash[s[r]]!=-1){
+          if(hash[s[r]]>=l){
+               l=hash[s[r]]+1;
+          }
+     }
+          int len=r-l+1;
+      maxlen=max(len,maxlen);
+      hash[s[r]]=r;
+      r++;
+      }
+      return maxlen;
+      
  }
 
  int main(){
